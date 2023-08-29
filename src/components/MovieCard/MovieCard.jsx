4cb0b378e10c423dcdb631de09150e1cbfbc513f
anagelberg <amy.nagelberg@gmail.com>
2023-08-28 22:20:08 -0700

    finish movie card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,23 +1,47 @@
 import "./MovieCard.scss";
 import { useState } from "react";
+import delIcon from "../../assets/icons/delete.svg";
+import editIcon from "../../assets/icons/edit.svg";
+import closeIcon from "../../assets/icons/close_dark.svg";
+import starIcon from "../../assets/icons/star.svg";
+//Attribution icons8
+//<a href="https://www.freepik.com/search?format=search&freeSvg=free&last_filter=freeSvg&last_value=free&query=star&type=icon">Icon by UIcons</a> star
 
 function MovieCard({ movie }) {
-  //   const [isFlipped, setIsFlipped] = useState(false);
-
   return (
-    <div
-      className="card"
-      //   onMouseOver={() => setIsFlipped(true)}
-      //   onMouseOut={() => setIsFlipped(false)}
-    >
+    <div className="card">
       <div className="card">
         <div className="card__front">
           <div className="card__overlay"></div>
           <img className="card__poster-img" src={movie.poster_url} alt="" />
         </div>
+        {/* Back */}
         <div className="card__back">
-          <h4>{movie.title}</h4>
-          <p className="card__description">{movie.description}</p>
+          <div className="card__icon-head">
+            <div>
+              <img className="card__icon" src={editIcon} alt="pencil icon" />
+              <img className="card__icon" src={delIcon} alt="trashcan icon" />
+            </div>
+            {/* <img className="card__icon" src={closeIcon} alt="X icon" /> */}
+          </div>
+
+          <div className="card__text">
+            <h4>
+              {movie.title} ({movie.year})
+            </h4>
+
+            <p className="card__rating">
+              <img className="card__icon" src={starIcon} alt="" />
+              {`${movie.rating} / 10`}
+            </p>
+            <div>
+              <p className="card__vibe">
+                {movie.emotional_vibe} emotionally, {movie.mental_vibe} mentally
+              </p>
+            </div>
+            <p className="card__description">{movie.description}</p>
+            <p className="card__runtime">{movie.run_time} minutes</p>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/DisplayJarPage/DisplayJarPage.jsx
+++ src/pages/DisplayJarPage/DisplayJarPage.jsx
@@ -9,12 +9,14 @@ const movie = {
     "https://www.themoviedb.org/t/p/w440_and_h660_face/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg",
   description:
     "Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.",
-  rating: 74,
+  rating: 7.4,
   year: 2023,
   run_time: 114,
   genres: ["comedy", "adventure", "fantasy"],
   imdb_id: "tt1517268",
   tmdb_id: 346698,
+  mental_vibe: "neutral",
+  emotional_vibe: "neutral",
 };
 
 function DisplayJarPage() {
@@ -23,10 +25,22 @@ function DisplayJarPage() {
   return (
     <>
       <div className="jar">
-        <h1>Movie Jar</h1>
+        <div className="jar__top-space">
+          <h1 className="jar__title">Movie Jar</h1>
+        </div>
+
         {/* <p>Show movies from jar with id of {params.jarid}</p> */}
         {/* <p>Example Movie Card</p> */}
-        <MovieCard movie={movie} />
+        <div className="jar__container">
+          <MovieCard movie={movie} />
+          <MovieCard movie={movie} />
+          <MovieCard movie={movie} />
+          <MovieCard movie={movie} />
+          <MovieCard movie={movie} />
+          <MovieCard movie={movie} />
+          <MovieCard movie={movie} />
+          <MovieCard movie={movie} />
+        </div>
       </div>
     </>
   );
