b3661d6434ca0f2500408f8032b6dee10a8e0fc2
anagelberg <amy.nagelberg@gmail.com>
2024-01-26 20:43:01 -0800

    add button tests

---
+++ src/components/Button/Button.test.jsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Button from './Button';
+
+
+test('renders Button component', () => {
+    render(<Button />);
+    expect(screen.getByRole('button')).toBeInTheDocument();
+});
+
+test('renders Button component with text', () => {
+    render(<Button text="test text" />);
+    expect(screen.getByText('test text')).toBeInTheDocument();
+});
+
+test('toggles Button modifier class', () => {
+    const { container } = render(<Button modifier="--testing-class" />);
+    expect(container.firstChild).toHaveClass('cta--testing-class')
+});
+
+test('Button calls clickHandler on click', () => {
+    const clickHandler = jest.fn();
+    render(<Button onClick={clickHandler} />);
+    userEvent.click(screen.getByRole('button'));
+    expect(clickHandler).toHaveBeenCalled();
+});
+
+test('Button is passed undefined props as default attributes', () => {
+    render(<Button disabled={true} />);
+    expect(screen.getByRole('button')).toBeDisabled();
+});
+
+
+// TODO: test icon matches snapshot
