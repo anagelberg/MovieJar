b1c0d375fd4a31b4d12f666132fb134fa2469446
anagelberg <amy.nagelberg@gmail.com>
2024-02-06 20:58:24 -0800

    add tests for MovieCardPreview component

--- src/components/MovieCardPreview/MovieCardPreview.jsx
+++ src/components/MovieCardPreview/MovieCardPreview.jsx
@@ -1,11 +1,10 @@
 import "./MovieCardPreview.scss";
 
 function MovieCardPreview({ movie, setSelectedMovie, selected }) {
-  return (
+  if (movie) return (
     <div className={selected ? "cardPrev cardPrev--selected" : "cardPrev"} onClick={() => { setSelectedMovie(movie) }}>
       <div className="card__overlay"></div>
       <img className="cardPrev__poster-img" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" />
-
     </div>
   );
 }
